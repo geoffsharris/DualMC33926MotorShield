349d9fdbe8d474aeebf3179c571d319ecf7906ad
geoffsharris <geoffsharris@yahoo.com>
2015-08-08 22:43:53 -0700

    Update uber-library-example.h

--- firmware/uber-library-example.h
+++ firmware/uber-library-example.h
@@ -1,36 +1,35 @@
-#ifndef _UBER_LIBRARY_EXAMPLE
-#define _UBER_LIBRARY_EXAMPLE
+#ifndef DualMC33926MotorShield_h
+#define DualMC33926MotorShield_h
 
-// Make library cross-compatiable
-// with Arduino, GNU C++ for tests, and Spark.
-//#if defined(ARDUINO) && ARDUINO >= 100
-//#include "Arduino.h"
-//#elif defined(SPARK)
-//#include "application.h"
-//#endif
+#include <application.h>
 
-// TEMPORARY UNTIL the stuff that supports the code above is deployed to the build IDE
-#include "application.h"
-namespace UberLibraryExample
+class DualMC33926MotorShield
 {
-  class Pin
-  {
-    private:
-      int number;
-      int mode;
-      bool state;
-    public:
-      Pin(int _number);
-      void beginInPinMode(PinMode _pinMode);
-      void modulateAtFrequency(int _ms);
-      int getNumber();
-      bool getState();
-      bool getMode();
-      bool isHigh();
-      void setHigh();
-      void setLow();
-      void setActualPinState();
-  };
-}
+  public:  
+    // CONSTRUCTORS
+    DualMC33926MotorShield(); // Default pin selection.
+    DualMC33926MotorShield(unsigned char M1DIR, unsigned char M1PWM, unsigned char M1FB,
+                           unsigned char M2DIR, unsigned char M2PWM, unsigned char M2FB,
+                           unsigned char nD2, unsigned char nSF); // User-defined pin selection. 
+    
+    // PUBLIC METHODS
+    void init(); // Initialize TIMER 1, set the PWM to 20kHZ. 
+    void setM1Speed(int speed); // Set speed for M1.
+    void setM2Speed(int speed); // Set speed for M2.
+    void setSpeeds(int m1Speed, int m2Speed); // Set speed for both M1 and M2.
+    unsigned int getM1CurrentMilliamps(); // Get current reading for M1. 
+    unsigned int getM2CurrentMilliamps(); // Get current reading for M2.
+    unsigned char getFault(); // Get fault reading.
+    
+  private:
+    unsigned char _nD2;
+    unsigned char _M1DIR;
+    unsigned char _M2DIR;
+    static const unsigned char _M1PWM = 9;
+    static const unsigned char _M2PWM = 10;
+    unsigned char _nSF;
+    unsigned char _M1FB;
+    unsigned char _M2FB;
+};
 
 #endif
