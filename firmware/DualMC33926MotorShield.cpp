0bf148487cd941a1f246a911cdac21e832db4f0e
geoffsharris <geoffsharris@yahoo.com>
2015-08-09 12:55:13 -0700

    Update DualMC33926MotorShield.cpp

--- firmware/DualMC33926MotorShield.cpp
+++ firmware/DualMC33926MotorShield.cpp
@@ -5,23 +5,35 @@
 DualMC33926MotorShield::DualMC33926MotorShield()
 {
   //Pin map
-  _nD2 = 4;
-  _M1DIR = 7;
-  _M2DIR = 8;
-  _nSF = 12;
+  _EN = D3;
+  _M1IN1 = D0;
+  _M1IN2 = D1;
+  _M2IN1;
+  _M2IN2;
+  _nSF = D2;
   _M1FB = A0; 
-  _M2FB = A1;
+  _M2FB;
 }
 
-DualMC33926MotorShield::DualMC33926MotorShield(unsigned char M1DIR, unsigned char M1PWM, unsigned char M1FB,
-                                               unsigned char M2DIR, unsigned char M2PWM, unsigned char M2FB,
-                                               unsigned char nD2, unsigned char nSF)
+(unsigned char M1IN1, unsigned char M1IN2, unsigned char M1FB,
+                           unsigned char M2IN1, unsigned char M2IN2, unsigned char M2FB,
+                           unsigned char EN, unsigned char nSF); // User-defined pin selection. 
+
+
+
+
+
+DualMC33926MotorShield::DualMC33926MotorShield(unsigned char M1IN1, unsigned char M1IN2, unsigned char M1FB,
+                           unsigned char M2IN1, unsigned char M2IN2, unsigned char M2FB,
+                           unsigned char EN, unsigned char nSF)
 {
   //Pin map
   //PWM1 and PWM2 cannot be remapped because the library assumes PWM is on timer1
-  _nD2 = nD2;
-  _M1DIR = M1DIR;
-  _M2DIR = M2DIR;
+  _EN = EN;
+  _M1IN1 = M1IN1;
+  _M1IN2 = M1IN2;
+  _M2IN1 = M2IN1;
+  _M2IN2 = M2IN2;
   _nSF = nSF;
   _M1FB = M1FB; 
   _M2FB = M2FB;
@@ -32,29 +44,16 @@ void DualMC33926MotorShield::init()
 {
 // Define pinMode for the pins and set the frequency for timer1.
 
-  pinMode(_M1DIR,OUTPUT);
-  pinMode(_M1PWM,OUTPUT);
+  pinMode(_M1IN1,OUTPUT);
+  pinMode(_M1IN2,OUTPUT);
   pinMode(_M1FB,INPUT);
-  pinMode(_M2DIR,OUTPUT);
-  pinMode(_M2PWM,OUTPUT);
+  pinMode(_M2IN1,OUTPUT);
+  pinMode(_M2IN1,OUTPUT);
   pinMode(_M2FB,INPUT);
-  pinMode(_nD2,OUTPUT);
-  digitalWrite(_nD2,HIGH); // default to on
+  pinMode(_EN,OUTPUT);
+  digitalWrite(_EN,HIGH); // default to on
   pinMode(_nSF,INPUT);
 
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__)
-  // Timer 1 configuration
-  // prescaler: clockI/O / 1
-  // outputs enabled
-  // phase-correct PWM
-  // top of 400
-  //
-  // PWM frequency calculation
-  // 16MHz / 1 (prescaler) / 2 (phase-correct) / 400 (top) = 20kHz
-  TCCR1A = 0b10100000;
-  TCCR1B = 0b00010001;
-  ICR1 = 400;
-  #endif
 }
 // Set speed for motor 1, speed is a number betwenn -400 and 400
 void DualMC33926MotorShield::setM1Speed(int speed)
@@ -66,17 +65,15 @@ void DualMC33926MotorShield::setM1Speed(int speed)
     speed = -speed;  // Make speed a positive quantity
     reverse = 1;  // Preserve the direction
   }
-  if (speed > 400)  // Max PWM dutycycle
-    speed = 400;
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__)
-  OCR1A = speed;
-  #else
-  analogWrite(_M1PWM,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif
+  if (speed > 255)  // Max PWM dutycycle
+    speed = 255;
+    
   if (reverse)
-    digitalWrite(_M1DIR,HIGH);
+    analoglWrite(_M1IN2,0);
+    analoglWrite(_M1IN1,speed);
   else
-    digitalWrite(_M1DIR,LOW);
+    analoglWrite(_M1IN1,0);
+    analoglWrite(_M1IN2,speed);
 }
 
 // Set speed for motor 2, speed is a number betwenn -400 and 400
@@ -89,17 +86,15 @@ void DualMC33926MotorShield::setM2Speed(int speed)
     speed = -speed;  // Make speed a positive quantity
     reverse = 1;  // Preserve the direction
   }
-  if (speed > 400)  // Max PWM dutycycle
-    speed = 400;
-  #if defined(__AVR_ATmega168__)|| defined(__AVR_ATmega328P__)
-  OCR1B = speed;
-  #else
-  analogWrite(_M2PWM,speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
-  #endif
+  if (speed > 255)  // Max PWM dutycycle
+    speed = 255;
+    
   if (reverse)
-    digitalWrite(_M2DIR,HIGH);
+    analoglWrite(_M2IN2,0);
+    analoglWrite(_M2IN1,speed);
   else
-    digitalWrite(_M2DIR,LOW);
+    analoglWrite(_M2IN1,0);
+    analoglWrite(_M2IN2,speed);
 }
 
 // Set speed for motor 1 and 2
@@ -110,17 +105,17 @@ void DualMC33926MotorShield::setSpeeds(int m1Speed, int m2Speed)
 }
 
 // Return motor 1 current value in milliamps.
-unsigned int DualMC33926MotorShield::getM1CurrentMilliamps()
+unsigned float DualMC33926MotorShield::getM1CurrentMilliamps()
 {
   // 5V / 1024 ADC counts / 525 mV per A = 9 mA per count
-  return analogRead(_M1FB) * 9;
+  return analogRead(_M1FB) * 1.5244;
 }
 
 // Return motor 2 current value in milliamps.
-unsigned int DualMC33926MotorShield::getM2CurrentMilliamps()
+unsigned float DualMC33926MotorShield::getM2CurrentMilliamps()
 {
   // 5V / 1024 ADC counts / 525 mV per A = 9 mA per count
-  return analogRead(_M2FB) * 9;
+  return analogRead(_M2FB) * 1.5244;
 }
 
 // Return error status
